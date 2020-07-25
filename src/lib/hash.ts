625260df05d5443b64562672b3a831c96d09b52f
Soichi Takamura <thepiglovesyou@gmail.com>
2020-07-25 10:47:57 +0900

    revert hash rule

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -4,6 +4,5 @@ export function createHash(s: string | Buffer): string {
   return crypto
     .createHash('sha1')
     .update((Buffer.isBuffer(s) ? s.toString() : s).replace(/\r\n/g, '\n'))
-    .digest('hex')
-    .slice(0, 6);
+    .digest('hex');
 }
